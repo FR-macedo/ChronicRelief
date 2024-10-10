dacda24b7bf179582264e5765dd805cd7094ff93
Francisco Macedo <macedo270701@gmail.com>
2024-10-10 12:10:45 -0300

    Adição da rota de Login de usuário

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -31,4 +31,31 @@ router.post('/register', async (req, res) => {
   }
 });
 
+// Rota para autenticar um usuário
+router.post('/login', async (req, res) => {
+    const { email, senha } = req.body;
+  
+    try {
+      // Encontra o usuário pelo email
+      const usuario = await User.findOne({ email });
+      if (!usuario) {
+        return res.status(404).json({ message: 'Usuário não encontrado.' });
+      }
+  
+      // Compara a senha fornecida com a senha armazenada
+      const senhaValida = await usuario.compararSenha(senha);
+      if (!senhaValida) {
+        return res.status(401).json({ message: 'Senha incorreta.' });
+      }
+  
+      // Gera um token JWT após a autenticação
+      const token = jwt.sign({ id: usuario._id }, process.env.JWT_SECRET, { expiresIn: '24h' });
+  
+      res.status(200).json({ message: 'Login bem-sucedido!', token });
+    } catch (error) {
+      console.error('Erro ao fazer login:', error);
+      res.status(500).json({ message: 'Erro ao fazer login.', error });
+    }
+  });
+
 module.exports = router;
